ee0210128ad0cc3ab237c73969a5b380d1219411
Safiya Naaz <safiya.f30@gmail.com>
2018-08-08 13:14:17 +0530

    reg

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
+$(function(){
+
+     $('#rollno').on('blur',function(){
+     	if(!/(\d{2}((co)|(DCO)|(EE)|(DEX)|(ME)|(DCES)|(DE)|(CE))\d{2,3})/i.test(this.value)){
+
+     		alert("Invalid Rollno");
+     		this.value="";
+     		$(this).focus();
+     	}
+     });
+
+     $('#sname').on('keypress',function(e){
+
+     		if(/[^a-zA-Z ]/.test(e.key)){
+     			alert("Invalid Name.ONly alphabets and space is allowed")
+     			this.value="";
+     			$(this).focus();
+     			return false;
+     		}
+
+     });
+
+     $('#address').on('blur',function(e){
+     	if(this.value.length>5){
+     		alert("length is greater than 150 or less than 50")
+     		this.value="";
+     		$(this).focus();
+     	}
+
+     });
+ 
+ $('#email').on('',function(){
+         if(/((\w*\d*\w*)@(\w*\d*))/)
 
 
+ });
 
-var element = document.getElementById('about');
-console.log(element.textContent);
+});
